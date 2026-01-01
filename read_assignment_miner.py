3246d1ef3039ac8fe932f7636f3c66193c0473ee
baka <sak24835@gmail.com>
2026-01-01 15:04:35 +0530

    second commit:Frontend done

--- read_assignment_miner.py
+++
@@ -1,9 +0,0 @@
-from pdfminer.high_level import extract_text
-
-try:
-    text = extract_text("Full Stack Web Developer Intern - Assignment.pdf")
-    print("---START-CONTENT---")
-    print(text)
-    print("---END-CONTENT---")
-except Exception as e:
-    print(f"Error reading PDF with pdfminer: {e}")
